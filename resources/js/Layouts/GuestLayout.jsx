eac24e88c0cec43341cf9316924e6ffe620793ee
rrrohithr <rohith@portrave.com>
2024-12-09 18:17:25 +0530

    login UI

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -4,15 +4,7 @@ import { Link } from '@inertiajs/react';
 export default function GuestLayout({ children }) {
     return (
         <div className="d-flex flex-column min-vh-100 align-items-center bg-light pt-5">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20" />
-                </Link>
-            </div>
-
-            <div className="mt-4 w-100 bg-white p-4 shadow-sm rounded-3">
-                {children}
-            </div>
+           {children}
         </div>
     );
 }
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -24,77 +24,29 @@ export default function Login({ status, canResetPassword }) {
     return (
         <GuestLayout>
             <Head title="Log in" />
-
-            {status && (
-                <div className="mb-4 text-sm font-medium text-green-600">
-                    {status}
+            <div className="col-12 col-sm-9 col-md-6 col-lg-4 col-xl-4 m-auto">
+                {status && (
+                    <div className="mb-4 text-sm font-medium text-success">
+                        {status}
+                    </div>
+                )}
+                <div className="bg-white p-4 rounded rounded-1">
+                    <div className="text-center mb-4">
+                        <div className="h3 fw-normal">Login to your account</div>
+                        <div>Login to your account using SSO</div>
+                    </div>
+                    <div>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <PrimaryButton className="w-100" disabled={processing}>
+                                Login using SSO
+                            </PrimaryButton>
+                        </div>
+                    </div>
                 </div>
-            )}
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
+                <div className="mt-3 text-center">
+                    Developed by <a href="" className="text-decoration-none">Transport Exchange Group</a>
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4 block">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData('remember', e.target.checked)
-                            }
-                        />
-                        <span className="ms-2 text-sm text-gray-600">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
-
-                <div className="mt-4 flex items-center justify-end">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
+            </div>
         </GuestLayout>
     );
 }
